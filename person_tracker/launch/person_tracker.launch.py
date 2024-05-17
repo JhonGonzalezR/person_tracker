ef7c172ca05156e60623d1bbfc1760c25c0bb5f4
Jhon Edward Gonzalez <98565218+JhonGonzalezR@users.noreply.github.com>
2024-05-17 17:54:51 -0500

    Add files via upload

---
+++ person_tracker/launch/person_tracker.launch.py
@@ -0,0 +1,24 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    LaunchDescription()
+
+    tracker = Node(
+        package="person_tracker",
+        executable="detect_person",
+        
+        
+
+    )
+    follow = Node(
+        package="person tracker",
+        executable="follow_person",
+        
+    )
+
+
+  
+
+    return LaunchDescription([tracker,
+                              follow])  
